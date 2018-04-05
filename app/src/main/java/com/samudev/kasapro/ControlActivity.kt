6afd86ac817d4e884dd1fde22fb574e7ad7e7af1
samuelsen <oyvinsam@stud.ntnu.no>
2018-04-05 19:19:12 +0200

    Init

---
+++ app/src/main/java/com/samudev/kasapro/ControlActivity.kt
@@ -0,0 +1,35 @@
+package com.samudev.kasapro
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_control.*
+
+class ControlActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_control)
+        setSupportActionBar(toolbar)
+
+        fab_add_device.setOnClickListener { view ->
+            Snackbar.make(view, resources.getString(R.string.not_implemented), Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_control, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
