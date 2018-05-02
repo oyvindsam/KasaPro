338b5ae9213eaebbac1d13f4f2b273350ef06927
samuelsen <oyvinsam@stud.ntnu.no>
2018-05-02 20:17:52 +0200

    Dynamic icon/text for tile, long click to go to app

--- app/src/main/java/com/samudev/kasapro/KasaProTileService.kt
+++ app/src/main/java/com/samudev/kasapro/KasaProTileService.kt
@@ -1,7 +1,8 @@
 package com.samudev.kasapro
 
+import android.graphics.drawable.Icon
+import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
-import android.util.Log
 import android.widget.Toast
 import com.samudev.kasapro.control.ControlPresenter
 import com.samudev.kasapro.model.Device
@@ -10,40 +11,52 @@ import com.samudev.kasapro.util.PreferencesUtil
 
 class KasaProTileService : TileService(), AsyncTaskCaller {
 
-    private val LOG_TAG = KasaProTileService::class.java.simpleName
-    private var device : Device? = null
-
+    /**
+     * Should try to get current device and set light to either 100% or OFF.
+     */
     override fun onClick() {
         super.onClick()
-        Log.v(LOG_TAG, "From pref: $device")
-        val lightOn = device?.lightOn ?: return Toast.makeText(this, "No device found, can't set light", Toast.LENGTH_SHORT).show()
-        if (lightOn) {
-            // Turn light off completely
-            device?.lightOn = false
-        } else {
-            device?.lightOn = true
-            device?.brightness = 100
-        }
-        Log.v(LOG_TAG, "Setting state: $device")
+        val device = PreferencesUtil.getKasaDevice(this) ?: return Toast.makeText(this, "No device found, can't set light", Toast.LENGTH_SHORT).show()
+        device.lightOn = !device.lightOn  // Toggle
+        if (device.lightOn) device.brightness = 100  // If light is going to be [on] -> turn brightness to 100
+
+        toggleTile(device.lightOn)  // toggle to new state
         ControlPresenter.AdjustLightStateAsync().execute(this, device)
         PreferencesUtil.saveKasaDevice(this, device)
     }
 
     override fun onTileAdded() {
         super.onTileAdded()
-        // TODO: check device state and set icon accordingly
-        Log.v(LOG_TAG, "onTileAdded---")
+        val device = PreferencesUtil.getKasaDevice(this) ?: return  // return if user has not added a device
+        toggleTile(device.lightOn)
     }
 
     override fun onStartListening() {
         super.onStartListening()
-        // TODO: change icon accordingly to device state (on/off). Do elsewhere since resource heavy?
-        device = PreferencesUtil.getKasaDevice(this) ?: return
-        Log.v(KasaProTileService::class.java.simpleName, "onStartListening---")
+        val device = PreferencesUtil.getKasaDevice(this) ?: return
+        toggleTile(device.lightOn)
+    }
+
+    private fun toggleTile(on: Boolean) {
+        val tile = qsTile
+        if (on) {
+            tile.icon = Icon.createWithResource(this, R.drawable.ic_tile_brightness_100)
+            tile.label = getString(R.string.tile_kasapro_on)
+            tile.state = Tile.STATE_ACTIVE
+        } else {
+            tile.icon = Icon.createWithResource(this, R.drawable.ic_tile_brightness_0)
+            tile.label = getString(R.string.tile_kasapro_off)
+            tile.state = Tile.STATE_INACTIVE
+        }
+        tile.updateTile()
     }
 
     override fun asyncFinished(any: Any?) {
-        val device = any as Device
-        Log.v(KasaProTileService::class.java.simpleName, "Light updated to: $device")
+        if (any is Device) {
+            // only toggle if in wrong state
+            if (qsTile.state == Tile.STATE_ACTIVE && !any.lightOn) toggleTile(false)
+            else if (qsTile.state == Tile.STATE_INACTIVE && any.lightOn) toggleTile(true)
+        }
+        else Toast.makeText(this, "Could not update light", Toast.LENGTH_SHORT).show()
     }
 }
